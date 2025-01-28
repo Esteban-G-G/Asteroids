52887a6c6f451dee9a3fc3bf43250d803d41b4b8
Esteban Gonzalez <estebangonzalez@Estebans-MacBook-Pro.local>
2025-01-27 20:28:43 -0800

    Added player movement and gorup manangement to the game loop

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import pygame
 from constants import *
+from player import Player
 
 def main ():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        dt = clock.tick(60) / 1000
+
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill((0, 0, 0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        
+
+        print(f"Delta time: {dt:.4f} seconds")
+
     pygame.quit()
     
-    
-    #print ("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
